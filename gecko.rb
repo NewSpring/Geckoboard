1a19e18dfaaa5b8ee814c2e8c76c9b8c8c8c8bb4
Drew Delianides <drew@delianides.com>
2013-12-16 12:33:36 -0500

    trying to catch API rate limit

--- gecko.rb
+++ gecko.rb
@@ -6,20 +6,23 @@ require "active_support/core_ext"
 get '/:form' do
   wufoo = WuParty::Form.new(params[:form],:account => ENV['ACCOUNT'], :api_key => ENV['API_KEY'])
   current = Time.new
-
-  #Current Year
-  year = wufoo.count(:system => true,
-                     :filters => [['DateCreated','Is_after', current.prev_year.end_of_year],
-                                  ['CompleteSubmission','Is_equal_to',1]])
-  #Current Month
-  current_month = wufoo.count(:system => true,
-                              :filters => [['DateCreated','Is_after',"#{current.prev_month.end_of_month.strftime("%Y-%m-%d 23:59:59")}"],
-                                           ['CompleteSubmission','Is_equal_to',1]])
-  #Previous Month
-  prev_month = wufoo.count(:system => true,
-                           :filters => [['DateCreated','Is_after',"#{current.prev_month.prev_month.end_of_month.strftime("%Y-%m-%d 23:59:59")}"], 
-                                        ['DateCreated','Is_before',"#{current.beginning_of_month.strftime("%Y-%m-%d 00:00:01")}"],
-                                        ['CompleteSubmission','Is_equal_to',1]])
+  begin
+    #Current Year
+    year = wufoo.count(:system => true,
+                       :filters => [['DateCreated','Is_after', current.prev_year.end_of_year],
+                                    ['CompleteSubmission','Is_equal_to',1]])
+    #Current Month
+    current_month = wufoo.count(:system => true,
+                                :filters => [['DateCreated','Is_after',"#{current.prev_month.end_of_month.strftime("%Y-%m-%d 23:59:59")}"],
+                                             ['CompleteSubmission','Is_equal_to',1]])
+    #Previous Month
+    prev_month = wufoo.count(:system => true,
+                             :filters => [['DateCreated','Is_after',"#{current.prev_month.prev_month.end_of_month.strftime("%Y-%m-%d 23:59:59")}"],
+                                          ['DateCreated','Is_before',"#{current.beginning_of_month.strftime("%Y-%m-%d 00:00:01")}"],
+                                          ['CompleteSubmission','Is_equal_to',1]])
+  rescue WuParty::HTTPError => e
+    [403, {'Content-Type' => 'application/json'}, e.message]
+  end
 
   json :item => [{:value => "#{year}", :text => "Total for #{current.strftime("%Y")}"},
                  {:value => "#{current_month}", :text => "Total for #{current.strftime("%B %Y")}"},
