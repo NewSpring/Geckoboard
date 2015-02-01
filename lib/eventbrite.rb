84bc23b64f2f38db1519c04cd59774893d90bd48
delianides <drew@delianides.com>
2015-02-01 15:14:22 -0500

    added total options

--- lib/eventbrite.rb
+++ lib/eventbrite.rb
@@ -22,5 +22,44 @@ module WIDGET
       # json :item => ticket
       json :item => "#{ticket['ticket']['quantity_sold']}", :min => { :value => 0 }, :max => { :value => "#{ticket['ticket']['quantity_available']}" }
     end
+
+    get '/students' do
+      eb_auth_tokens = {
+        app_key: ENV['EB_APP_KEY'],
+        user_key: ENV['EB_USER_KEY']
+      }
+
+      eb_client = EventbriteClient.new(eb_auth_tokens)
+      response = eb_client.user_list_events({ only_display: "tickets,id", event_statuses: "live" })
+      quantity_sold = 0
+      response['events'].each do |e|
+        if e['id'] == "14147546693" then
+          break
+        end
+
+        e['event']['tickets'].each do |t|
+          quantity_sold = quantity_sold + t['ticket']['quantity_sold']
+        end
+      end
+
+      json :item => { :value => "#{quantity_sold}", :text => "Total Number of Students" }
+    end
+
+    get '/volunteers' do
+      eb_auth_tokens = {
+        app_key: ENV['EB_APP_KEY'],
+        user_key: ENV['EB_USER_KEY']
+      }
+
+      eb_client = EventbriteClient.new(eb_auth_tokens)
+      response = eb_client.event_get({ only_display: "tickets", event_statuses: "live", id: "14147546693" })
+      tickets = response['event']['tickets']
+      quantity_sold = 0
+      tickets.each do |t|
+        quantity_sold = quantity_sold+t['ticket']['quantity_sold'].to_i
+      end
+
+      json :item => { :value => "#{quantity_sold}", :text => "Total Number of Volunteers" }
+    end
   end
 end
